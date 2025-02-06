571e41a61bd368de161634da65c385ffe1b0db5b
plewd <samuel.hirn@protonmail.com>
2025-02-05 22:48:53 -0500

    Revert "Finish until 2.4.7"
    This reverts commit 0c947a8c2388c330bde3a4363896ac1d3a8ed8e4.
    Refresh branch back to what it was at the end of a1

--- src/Kambaz/Courses/Home/index.tsx
+++ src/Kambaz/Courses/Home/index.tsx
@@ -2,13 +2,14 @@ import Modules from "../Modules";
 import CourseStatus from "./Status";
 export default function Home() {
     return (
-        <div className="d-flex" id="wd-home">
-            <div className="flex-fill me-3">
-                <Modules/>
-            </div>
-            <div className="d-none d-xl-block">
-                <CourseStatus/>
-            </div>
-        </div>
-    );
-}
+        <table id="wd-home">
+            <tr>
+                <td valign="top">
+                    <Modules />
+                </td>
+                <td valign="top">
+                    <CourseStatus />
+                </td>
+            </tr>
+        </table>
+    );}
