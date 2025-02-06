45cb16440a177947f17758bc22d3d3fc12c4b721
plewd <samuel.hirn@protonmail.com>
2025-02-05 23:00:00 -0500

    Add id to Github link element

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -16,7 +16,7 @@ export default function TOC() {
                 <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="https://github.com/plewd">My GitHub</Nav.Link>
+                <Nav.Link id="wd-github" href="https://github.com/plewd">My GitHub</Nav.Link>
             </Nav.Item>
         </Nav>
     );}
