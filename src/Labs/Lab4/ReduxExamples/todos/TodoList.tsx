14ab43c424dba073049b5d117a38adc4b708849c
plewd <samuel.hirn@protonmail.com>
2025-03-12 15:38:03 -0400

    Fix bugs with Redux

--- src/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
@@ -9,8 +9,8 @@ export default function TodoList() {
             <h2>Todo List</h2>
             <ListGroup>
                 <TodoForm />
-                {todos.map((todo: any) => (
-                    <TodoItem todo={todo} />
+                {todos.map(() => (
+                    <TodoItem />
                 ))}
             </ListGroup>
             <hr/>
